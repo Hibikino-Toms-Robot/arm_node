02b6d15531eef4eac811860779ed1ddc3707f85e
Shuchaan <104504380+Shuchaan@users.noreply.github.com>
2023-06-14 17:31:54 +0900

    Update y_axis_control_misumi.py

--- y_axis_control_misumi.py
+++ y_axis_control_misumi.py
@@ -1,85 +1,75 @@
 import serial
 import time
 
-# x_axis_armは移動位置＝20-動かしたい位置だけ動かせばいい、1cm動かしたいときは、20-1=190.00[mm]
 # https://jp.misumi-ec.com/maker/misumi/mech/special/actuator_portal/rs/download/pdf/C1C21C22_JP_V201.pdf
 # p230より
 
 class Y_Axis_Control:
     """
     input:
-        Flag: 'I'→Initialize_mode 'T'→Target_mode
-        Target: y
-    (example1) Flag = 'I' 初期化モード
-    (example2) Flag = 'T' Target = 20000 ターゲットモード　目標位置を20.000[cm]に設定（Z軸と合わせるために少数第一位で四捨五入してもいいかも？）
-
+        Target Location
     output:
-        finish_flag → 0
-
+        finish flag
     """
-    def __init__(self, Flag, Target):
+    def __init__(self):
         self.ser = serial.Serial("COM8", baudrate=38400, bytesize=8, parity=serial.PARITY_ODD, stopbits=1, xonxoff=False)
-        self.Flag = Flag
-        self.Target = Target
 
-    def Servo_On(self):
+    def servo_on(self):
         self.ser.write(b'@SRVO1,') # サーボON指令
-        On_flag = True
-        while On_flag:
+        while True:
             self.ser.write(b'@?OPT1,') # オプション情報（状態）の読み出し
             receive = self.ser.readline()
             if receive == b'OK.1\r\n':
-                time.sleep(0.1)
+                time.sleep(0.1) # 0.1秒停止
                 break
 
-    def Servo_Off(self):
+    def servo_off(self):
         self.ser.write(b'@SRVO0,') # サーボOFF指令
         time.sleep(0.1)
 
-    def Org_Arm(self):
-        self.ser.write(b'@ORG,') # 原点回帰指令
-        org_flag = True
-        while org_flag:
-            self.ser.write(b'@?OPT1,')
+    def org_arm(self):
+        self.ser.write(b'@ORG,') # 限定回帰指令
+        while True:
+            self.ser.write(b'@?OPT1,') # オプション情報（状態）の読み出し
             receive = self.ser.readline()
+            # 2584, 2508, 2568 が終了応答
             if receive == b'OPT1.1=2584\r\n' or receive == b'OPT1.1=2508\r\n' or receive == b'OPT1.1=2568\r\n':
-                time.sleep(0.1)
-                break
-
-    def Target_Arm(self, Speed, Target_pos):
-        self.ser.write(('@S1='+str(Speed)+',').encode(encoding='utf-8')) # Speedの設定
-        time.sleep(0.1)
-        self.ser.write(('@START1#P'+str(Target_pos)+',').encode(encoding='utf-8')) #目標位置へ移動指令
-        target_flag = True
-        while target_flag:
-            self.ser.write(b'@?OPT1,')
-            receive = self.ser.readline()
-            if receive == b'OPT1.1=2570\r\n' or receive == b'OPT1.1=2346\r\n':
-                time.sleep(0.1)
+                time.sleep(0.1) # 0.1秒停止
                 break
     
-    def main(self):
-        # 初期化モード
-        if self.Flag == 'I':
-            self.Servo_On()
-            self.Org_Arm()
-            self.Target_Arm(100, 10000) #初期化後は中心位置
-        # ターゲットモード
-        elif self.Flag == 'T':
-            self.Target_Arm(100, self.Target)
+    def move_init(self):
+        self.servo_on()
+        self.org_arm()
+        self.move_target(10000)
+        return 0
 
-        self.ser.close()
+    def move_target(self, target):
+        self.ser.write(('@S1=100,').encode(encoding='utf-8')) # Speedの設定
+        time.sleep(0.1) # 0.1秒停止
+        self.ser.write(('@START1#P'+str(target)+',').encode(encoding='utf-8')) #目標位置へ移動指令
+        while True:
+            self.ser.write(b'@?OPT1,') # オプション情報（状態）の読み出し
+            receive = self.ser.readline()
+            # 2570, 2346が終了応答
+            if receive == b'OPT1.1=2570\r\n' or receive == b'OPT1.1=2346\r\n':
+                time.sleep(0.1) # 0.1秒停止
+                break
+        # time.sleep(1)
 
         return 0
     
-# デバック
-Flag = 'I'
-Y_target = None
-ini = Y_Axis_Control(Flag, Y_target)
-fin = ini.main()
-
-Flag = 'T'
-Y_target = 10000
-tar = Y_Axis_Control(Flag, Y_target)
-fin = tar.main()
-print(fin)
+'''デバック'''
+y_control = Y_Axis_Control()
+# 初期位置へ移動
+ini = y_control.move_init()
+# 目標位置へ移動
+tag1 = y_control.move_target(20000)
+time.sleep(2)
+tag2 = y_control.move_target(10000)
+time.sleep(2)
+tag3 = y_control.move_target(00000)
+time.sleep(2)
+tag4 = y_control.move_target(20000)
+time.sleep(2)
+tag5 = y_control.move_target(00000)
+time.sleep(2)
